e90d245b4ec0a8d13d3ed5cb4fe460007c0de634
abdullahmustapha59@gmail.com <abdullahmustapha59@gmail.com>
2022-01-31 22:09:55 +0000

    all about the basics of python

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+firstName = 'Amasap'
+secondName = 'Dasty'
+print(firstName + " "+ secondName[0].lower())
+print(len(firstName))
+print(3*firstName)
+
+# List
+scoreBoard = ['amAsap', 'Dasty', 2.2, 90]
+print(scoreBoard)
+print(scoreBoard[2])
+print(scoreBoard[0] + " "+ scoreBoard[1])
+#tuple
+tup = (20, 30)
+(a, b) = tup
+tup1 = (a, b)
+addTup = tup + tup1
+print(tup1)
+print(addTup)
+print(b + a)
+
